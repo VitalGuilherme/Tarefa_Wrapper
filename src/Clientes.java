eda4f43d3884ed17c97ee22ff07ffe89dc0476a7
Vital.Guilherme <guilhermev.j.santos@gmail.com>
2024-01-13 15:52:24 -0300

    tarefa

---
+++ src/Clientes.java
@@ -0,0 +1,11 @@
+public class Clientes {
+
+    public static void main (String args []) {
+
+        long cpfClientes = 15620345890l;
+        System.out.println(cpfClientes);
+        Long cpfClientes1 = 48236987514L;
+        System.out.println(cpfClientes1);
+    }
+
+}
